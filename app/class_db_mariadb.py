1a8d0674f28826d03cfbd05714c4ef35d0bc1ecf
Luc Franco <luc.framco@wanadoo.fr>
2020-05-02 00:03:25 +0200

    liste lignes

--- app/class_db_mariadb.py
+++ app/class_db_mariadb.py
@@ -76,7 +76,7 @@ class gestionMARIADB:
     def listStationLigne(self, var_ligne):
         print('listStationLigne')
         list_station_lgn = self.mariadb.cursor()
-        request = ("SELECT tr.route_id, tr.trip_id, s_tr.stop_id, s_tr.stop_sequence, sts.stop_name, sts.stop_desc, sts.stop_lat, sts.stop_lon FROM trips AS tr LEFT JOIN stop_times AS s_tr ON tr.trip_id = s_tr.trip_id LEFT JOIN stops AS sts ON sts.stop_id = s_tr.stop_id LEFT JOIN routes AS rtes ON rtes.route_id = tr.route_id WHERE tr.route_id = " + var_ligne + " GROUP BY stop_name ORDER BY s_tr.stop_sequence")
+        request = ("SELECT tr.route_id, tr.trip_id, s_tr.stop_id, s_tr.stop_sequence, sts.stop_name, sts.stop_desc, sts.stop_lat, sts.stop_lon FROM trips AS tr LEFT JOIN stop_times AS s_tr ON tr.trip_id = s_tr.trip_id LEFT JOIN stops AS sts ON sts.stop_id = s_tr.stop_id LEFT JOIN routes AS rtes ON rtes.route_id = tr.route_id WHERE tr.route_id = " + str(var_ligne) + " GROUP BY stop_name ORDER BY s_tr.stop_sequence")
         print("0| " + request)
         list_station_lgn.execute(request)
         records = list_station_lgn.fetchall()
