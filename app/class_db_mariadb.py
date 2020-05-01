0d37af094f5e50b8685d62af0c8254d61e9d6cee
Luc Franco <luc.framco@wanadoo.fr>
2020-05-01 23:47:06 +0200

    liste lignes

--- app/class_db_mariadb.py
+++ app/class_db_mariadb.py
@@ -76,16 +76,16 @@ class gestionMARIADB:
     def listStationLigne(self, var_ligne):
         print('listStationLigne')
         list_station_lgn = self.mariadb.cursor()
-        request = ("""SELECT tr.route_id, tr.trip_id, s_tr.stop_id, s_tr.stop_sequence, sts.stop_name, sts.stop_desc, sts.stop_lat, sts.stop_lon
+        request = """SELECT tr.route_id, tr.trip_id, s_tr.stop_id, s_tr.stop_sequence, sts.stop_name, sts.stop_desc, sts.stop_lat, sts.stop_lon
                         FROM trips AS tr
                         LEFT JOIN stop_times AS s_tr ON tr.trip_id = s_tr.trip_id
                         LEFT JOIN stops AS sts ON sts.stop_id = s_tr.stop_id
                         LEFT JOIN routes AS rtes ON rtes.route_id = tr.route_id
                         WHERE tr.route_id = ?
                         GROUP BY stop_name
-                        ORDER BY s_tr.stop_sequence""", var_ligne)
+                        ORDER BY s_tr.stop_sequence"""
         print("0| " + request)
-        list_station_lgn.execute(request)
+        list_station_lgn.execute(request, var_ligne)
         records = list_station_lgn.fetchall()
         self.nb_list_station_lgn = list_station_lgn.rowcount
         return records
