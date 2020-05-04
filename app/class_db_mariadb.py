bf92da7aaf04a5a80006dafeffece9b4566460ff
Luc Franco <luc.framco@wanadoo.fr>
2020-05-04 03:28:05 +0200

    station ajax

--- app/class_db_mariadb.py
+++ app/class_db_mariadb.py
@@ -84,10 +84,10 @@ class gestionMARIADB:
         self.nb_list_station_lgn = list_station_lgn.rowcount
         return records
 
-    def infoStation(self, ligne, lat, lng):
+    def infoStation(self, lat, lng):
         print('infoStation')
         infostation = self.mariadb.cursor()
-        request = ("SELECT rtes.route_short_name, rtes.route_id,  tr.trip_short_name, sts.stop_id, sts.stop_name, sts.stop_desc FROM routes AS rtes LEFT JOIN trips AS tr ON rtes.route_id = tr.route_id LEFT JOIN stop_times AS s_tr ON tr.trip_id = s_tr.trip_id LEFT JOIN stops AS sts ON sts.stop_id = s_tr.stop_id WHERE s_tr.stop_id = (SELECT DISTINCT s_tr.stop_id FROM trips AS tr LEFT JOIN stop_times AS s_tr ON tr.trip_id = s_tr.trip_id LEFT JOIN stops AS sts ON sts.stop_id = s_tr.stop_id LEFT JOIN routes AS rtes ON rtes.route_id = tr.route_id WHERE tr.route_id = " + str(ligne) + " AND sts.stop_lat = " + str(lat) + " AND sts.stop_lon = " + str(lng) + ") GROUP BY rtes.route_short_name")
+        request = ("SELECT sts2.stop_id, sts2.stop_name, sts2.stop_desc, s_tr.stop_sequence, rtes.route_id, rtes.route_short_name, rtes.route_type FROM stops AS sts2 LEFT JOIN stop_times AS s_tr ON s_tr.stop_id = sts2.stop_id LEFT JOIN trips AS tr ON tr.trip_id = s_tr.trip_id LEFT JOIN routes AS rtes ON rtes.route_id = tr.route_id WHERE sts2.stop_name = (SELECT DISTINCT sts1.stop_name FROM stops AS sts1 WHERE sts1.stop_lat = " + str(lat) + " AND sts1.stop_lon = " + str(lng) + " GROUP BY rtes.route_short_name")
         print("0| " + request)
         infostation.execute(request)
         records = infostation.fetchall()
