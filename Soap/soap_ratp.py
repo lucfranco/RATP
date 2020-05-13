c04347263485c1c98a41b54f58bde9e0d4b41a56
Luc Franco <luc.framco@wanadoo.fr>
2020-05-14 00:59:14 +0200

    soap ratp

--- Soap/soap_ratp.py
+++ Soap/soap_ratp.py
@@ -29,4 +29,5 @@ soap_ratp = soapRATP(soap_config, path_base)
 
 
 if __name__ == "__main__":
-    print('main')
+    missions = stop_horaire('M13', 'Mairie de Saint-Ouen', 'A', [])
+    print(missions)
--- class_python/class_soap_ratp.py
+++ class_python/class_soap_ratp.py
@@ -7,9 +7,7 @@ class soapRATP:
         self.config = config
         self.path_base = path_base
         try:
-
             ratp_client = Client(self.path_base + '/Soap/' + self.config["wsdl"])
-
             line_t = ratp_client.get_type('ns0:Line')
             station_t = ratp_client.get_type('ns0:Station')
             direction_t = ratp_client.get_type('ns0:Direction')
@@ -18,3 +16,17 @@ class soapRATP:
             print('erreur de connexion soap RATP')
         else:
             print("soap RATP OK !!")
+
+def stop_horaire(line, station, sens, stops):
+    ratp_horaire = {}
+
+    oline = line_t(id=line)
+    ostation = station_t(line = oline, name=station)
+    odirection = direction_t(sens = sens)
+    missions = ratp_client.service.getMissionsNext(station=ostation, direction=odirection)
+    ratp_horaire = {
+        'line' = missions['argumentDirection']['line']['code']
+        'name' = missions['argumentDirection']['name']
+        'sens' = missions['argumentDirection']['sens']
+    }
+    return ratp_horaire
