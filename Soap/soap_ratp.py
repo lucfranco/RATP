8e1a23df723a621ff6e39f9837f48ee6d558f72e
Luc Franco <luc.framco@wanadoo.fr>
2020-05-14 04:52:38 +0200

    soap ratp

--- Soap/soap_ratp.py
+++ Soap/soap_ratp.py
@@ -4,24 +4,17 @@ import os
 import sys
 from pathlib import Path
 from dotenv import load_dotenv
-
 import avro.schema
-from avro.datafile import DataFileReader, DataFileWriter
 from avro.io import DatumReader, DatumWriter
 
-
-# Path file .env-----------------
+# Path file .env / Class-----------------
 path_origin = str(os.path.abspath(sys.argv[0]))[1:].split('/')
-#print("path_origin = ", path_origin, type(path_origin))
 path_base = '/' + '/'.join(path_origin[0:(path_origin.index('RATP') + 1)])
-#print("path_base = ", path_base, type(path_base))
 
 path_env = path_base + '/.env'
-#print("path_env = ", path_env, type(path_env))
 load_dotenv(dotenv_path=path_env)
 
 path_import = path_base + '/class_python'
-#print("path_import = ", path_import, type(path_import))
 sys.path.insert(0, path_import)
 
 from class_soap_ratp import soapRATP
@@ -29,15 +22,25 @@ from class_soap_ratp import soapRATP
 soap_config = {
     'wsdl': os.getenv("wsdl_file")
 }
-
 soap_ratp = soapRATP(soap_config, path_base)
+#---------------------------------------------
 
+# Path Shema AVRO --------------------------------------------
 path_shema = path_base + '/Soap/'  + 'stop_horaire.avsc'
 schema = avro.schema.Parse(open(path_shema).read())
 
 if __name__ == "__main__":
     missions = soap_ratp.stop_horaire('M13', 'Mairie de Saint-Ouen', 'A', [])
-    print(missions)
+
+    SCHEMA = avro.schema.Parse(open(path_shema).read())
+    writer = DatumWriter(SCHEMA)
+    bytes_writer = io.BytesIO()
+    encoder = avro.io.BinaryEncoder(bytes_writer)
+    writer.write(missions, encoder)
+    raw_bytes = bytes_writer.getvalue()
+    print(raw_bytes)
+'''
+
     writer = DataFileWriter(open("users.avro", "wb"), DatumWriter(), schema)
     writer.append(missions)
     writer.close()
@@ -46,4 +49,4 @@ if __name__ == "__main__":
     for user in reader:
         print(user)
     reader.close()
-
+'''
