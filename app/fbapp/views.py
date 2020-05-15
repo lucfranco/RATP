358fb3947c290ce447a57d47257a355db0e88dd2
Luc Franco <luc.framco@wanadoo.fr>
2020-05-15 09:14:48 +0200

    soap

--- app/fbapp/views.py
+++ app/fbapp/views.py
@@ -137,16 +137,16 @@ def cartecassandra():
 def stations_ligne_cassandra(ligne):
     ratp_cassandra = gestionCASSANDRA(cassandra_config)
     list_stations_ligne = ratp_cassandra.listStationLigne(ligne)
-    print(list_stations_ligne[0][10], len(list_stations_ligne[0][10]))
-    print(list_stations_ligne[0][11], len(list_stations_ligne[0][11]))
-    print(list_stations_ligne[0][12], len(list_stations_ligne[0][12]))
+    #print(list_stations_ligne[0][10], len(list_stations_ligne[0][10]))
+    #print(list_stations_ligne[0][11], len(list_stations_ligne[0][11]))
+    #print(list_stations_ligne[0][12], len(list_stations_ligne[0][12]))
     list_stations_dict = dict()
     list_stations_dict['stations'] = list()
     list_stations_dict['LIGNE_SHORT_NAME'] = list_stations_ligne[0][1]
     list_stations_dict['LIGNE_ID'] = list_stations_ligne[0][0]
 
     for i in range(0, len(list_stations_ligne[0][7])-1):
-        print(i)
+        #print(i)
         list_stations_dict['stations'].append({
             'ID': list_stations_ligne[0][8][i],
             'SEQUENCE': list_stations_ligne[0][12][i],
